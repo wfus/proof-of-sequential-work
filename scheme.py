200eb0368776731ed6e9fc4dacc3a817620a71b6
WFU <wfu@college.harvard.edu>
2018-04-21 22:02:28 -0400

    [scheme] added nonce for sha

--- scheme.py
+++ scheme.py
@@ -27,6 +27,19 @@ def sha256(x):
     h.update(str(x).encode('utf-8'))
     return int(h.hexdigest(), 16)
 
+"""
+Takes in two integers, a nonce and x, to serve as our oracle function.
+This can be replaced with another function that can be used.  
+"""
+def sha256H(nonce, x):
+    h = hashlib.sha256()
+    first = str(nonce).encode('utf-8')
+    second = str(x).encode('utf-8')
+    h.update(first+second)
+    return int(h.hexdigest(), 16) 
+
+
+
 
 DEFAULT_w = 10
 DEFAULT_t = 2**10 - 1
@@ -49,12 +62,19 @@ of random gamma_1, ..., gamma_t sampled from (0, 1)^w
 def opening_challenge(w=DEFAULT_w, t=DEFAULT_t, N=DEFAULT_N):
     return [random.randint(0, 2**w - 1) for i in range(t)]
 
-
-def open():
+"""
+Prover computes tau := open^H(chi, N, phi_P, gamma) and sends it to 
+the Verifier
+"""
+def open(chi, phi_P, gamma, N=DEFAULT_N):
     raise NotImplementedError  
 
-
-def verify():
+"""
+Verifier computes and outputs verify^H(chi, N, phi, gamma, tau)
+given either {accept, reject}
+We will let accept be True and reject be False
+"""
+def verify(chi, phi, gamma, tau, N=DEFAULT_N):
     raise NotImplementedError 
 
 
@@ -62,6 +82,12 @@ def verify():
 def random_tests(): 
     print("Selecting from (0, 1)^1")
     print(opening_challenge(t=10))
+
+    print(sha256H(1, 10))
+    print(sha256H(11, 0))
+    print(sha256H(100, 1))
+    print(sha256H(11, 100))
+
 
 if __name__ == '__main__':
     random_tests() 
