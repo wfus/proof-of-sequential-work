79388027e94064ad6b112b33a8dc1dbc0d61e1fd
WFU <wfu@college.harvard.edu>
2018-04-23 03:18:13 -0400

    Added timing

--- timing.py
+++ timing.py
@@ -3,9 +3,11 @@ from util import sha256, sha256H
 import matplotlib.pyplot as plt
 import time
 
+MAX_N_LEN = 20 
+
 time_posw = []
 time_verify = []
-for n in range(1, 15):
+for n in range(1, MAX_N_LEN):
     chi = statement()
     start = time.time()
     G = compute_posw(chi, n=n)
@@ -21,7 +23,13 @@ for n in range(1, 15):
     time_verify.append(diff)
 print(time_posw)
 print(time_verify)
-plt.plot(range(1, 15), time_posw)
+plt.title("POSW compute time vs DAG depth ")
+plt.xlabel("n")
+plt.ylabel("Time (s)")
+plt.plot(range(1, MAX_N_LEN), time_posw)
 plt.show()
-plt.plot(range(1, 15), time_verify)
+plt.title("Verification time vs DAG depth")
+plt.xlabel("n")
+plt.ylabel("Time (s)")
+plt.plot(range(1, MAX_N_LEN), time_verify)
 plt.show()
